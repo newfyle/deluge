8c6d6fafcb3b9977421c689b703c5aec52da209d
newfyle <43925353+newfyle@users.noreply.github.com>
2018-10-13 20:31:07 +0800

    documented deluge rpc connection initializer bot

--- delugeBot.py
+++ delugeBot.py
@@ -26,6 +26,7 @@ class DelugeConnect:
         self.password=password
         
     def connect(self):
+        #connect based on the host:port and username and password
         self.client = DelugeRPCClient(self.host, self.port, self.username, self.password, automatic_reconnect=True)
         #please recommend everyone else to make a proper client username and password
         try:
@@ -39,7 +40,7 @@ class DelugeConnect:
         torrent=open(filename,'r').read()
         print(torrent)
         metadata = base64.b64encode(torrent)
-        #self.call_retry(self.client,'core.add_torrent_file',filename,metadata,{})
+        #this function is to add torrent based on the existing torrent file in the name of add_torrent
         return self.client.core.add_torrent_file(filename,metadata,{})
                 
     def get_torrent_status(self):
@@ -48,6 +49,7 @@ class DelugeConnect:
         for a in raw_progress.items():
             list_of_progress.append(a[1])
         return list_of_progress
+        #this function is to get the torrent status from a specified ip address
             
     def disconnect(self):
         try:
@@ -56,14 +58,4 @@ class DelugeConnect:
         except Exception as e:
             print(e)
             return "Cannot be disconnected, maybe it's already disconnected?"
-    
-
-
-# In[ ]:
-
-
-"""for a in client_connect.get_torrent_status().items():
-    print(type(a))
-    printp.pprint(a[1]['name']+" "+str(a[1]['progress']))
-    print("break")"""
 
